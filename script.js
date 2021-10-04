600e6510bcc1e56687819cb165e23618c52e4859
ob <oliviabennington@gmail.com>
2021-10-03 22:29:50 -0400

    script for hamburger, about page responsive, general code cleanup

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  function navData() {
+    var X = document.getElementById("navList");
+    if (X.style.display === "block") {
+      X.style.display = 'none';} else {
+        X.style.display= 'block';
+      }
+
+    }
