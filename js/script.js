e4fae19b59e20d7bf6691d06033569f95b974570
ob <oliviabennington@gmail.com>
2021-11-07 22:13:40 -0500

    fixing lightbox issue, preparing to host

--- js/script.js
+++ js/script.js
@@ -83,21 +83,3 @@
 //   slides[slideIndex - 1].style.display = 'block';
 //   modalPreviews[slideIndex - 1].className += ' active';
 // }
-
-$(document).ready(function () {
-    "use strict";
-    $(".lightbox").click(function () {
-        var imgsrc = $(this).attr('src');
-        $("body").append("<div class='img-popup'><span class='close-lightbox'>&times;</span><img src='" + imgsrc + "'></div>");
-        $(".close-lightbox, .img-popup").click(function () {
-            $(".img-popup").fadeOut(500, function () {
-                $(this).remove();
-            }).addClass("lightboxfadeout");
-        });
-
-    });
-    $(".lightbox").click(function () {
-        $(".img-popup").fadeIn(500);
-    });
-
-});
